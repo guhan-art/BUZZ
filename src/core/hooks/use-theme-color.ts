b8c1eb9d6966aead1b9be60c47a884498a76acfd
Guhan-alt <guhanrajs2607@gmail.com>
2026-08-15 21:53:22 +0530

    Fix remaining missing hooks imports from UI component refactoring

--- src/core/hooks/use-theme-color.ts
+++ src/core/hooks/use-theme-color.ts
@@ -2,7 +2,7 @@
  * Learn more about light and dark modes:
  * https://docs.expo.dev/guides/color-schemes/
  */
-import { Colors } from '../constants/theme';
+import { Colors } from '../theme/theme';
 import { useColorScheme } from './use-color-scheme';
 
 type Theme = 'light' | 'dark';
